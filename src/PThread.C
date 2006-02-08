b716983b06bb48c673d79af88e8ea788b7d7b561
Eric Anderson <eric.anderson4@hp.com>
2006-02-08 19:16:11 +0000

    * Import from CVS as of 2006/02/08 19:11:00

---
+++ src/PThread.C
@@ -0,0 +1,60 @@
+/* -*-C++-*-
+/*
+   (c) Copyright 2001-2005, Hewlett-Packard Development Company, LP
+
+   See the file named COPYING for license details
+*/
+
+/** @file
+    PThread implementation
+*/
+
+#include <PThread.H>
+
+PThread::PThread()
+{
+    int ret = pthread_attr_init(&attr);
+    AssertAlways(ret == 0,("pthread_attr_init failed: %s\n",strerror(ret)));
+    memset(&last_tid,sizeof(last_tid),0);
+}
+
+PThread::~PThread()
+{
+}
+
+void
+PThread::setDetached(bool detached)
+{
+    int ret = pthread_attr_setdetachstate(&attr, detached ? PTHREAD_CREATE_DETACHED : PTHREAD_CREATE_JOINABLE);
+    AssertAlways(ret == 0,("pthread_attr_setdetachstate failed: %s\n",strerror(ret)));
+}
+
+static void *
+pthread_starter(void *arg)
+{
+    PThread *obj = (PThread *)arg;
+    return obj->run();
+}
+
+pthread_t
+PThread::start()
+{
+    pthread_t tid;
+    int ret = pthread_create(&tid,&attr,pthread_starter,this);
+
+    AssertAlways(ret == 0,("pthread_create failed: %s",strerror(ret)));
+    
+    last_tid = tid;
+    return tid;
+}
+
+void *
+PThread::join()
+{
+    void *retval = NULL;
+    int tmp = pthread_join(last_tid, &retval);
+    AssertAlways(tmp == 0,("pthread_join failed: %s\n",strerror(tmp)));
+    return retval;
+}
+
+
---
+++ src/StatsEMA.C
@@ -0,0 +1,100 @@
+/* -*-C++-*-
+/*
+   (c) Copyright 2001-2005, Hewlett-Packard Development Company, LP
+
+   See the file named COPYING for license details
+*/
+
+/** @file
+    StatsEMA implementation
+*/
+
+#include <StatsEMA.H>
+#include <LintelAssert.H>
+
+using namespace std;
+
+StatsEMA::StatsEMA(double decay_rate)
+{
+    vector<double> decay_rates;
+    decay_rates.push_back(decay_rate);
+    init(decay_rates);
+}
+
+StatsEMA::StatsEMA(const vector<double> &decay_rates)
+{
+    init(decay_rates);
+}
+
+StatsEMA::~StatsEMA()
+{
+}
+
+void
+StatsEMA::reset()
+{
+    AssertFatal(("unimplemented"));
+}
+
+void
+StatsEMA::add(const double value)
+{
+    Stats::add(value);
+    for(unsigned i = 0;i<ema_values.size();++i) {
+	if (count_low[i]) {
+	    if ((double)countll() > 1.0 / decay_rates[i]) {
+		count_low[i] = false;
+		updateEMA(i,value);
+	    } else {
+		ema_values[i] = mean();
+	    }
+	} else {
+	    updateEMA(i,value);
+	}
+    }
+}
+
+double
+StatsEMA::getEMA(unsigned ema_num)
+{
+    AssertAlways(ema_num < ema_values.size(),
+		 ("invalid ema_num %d specified\n",ema_num));
+    return ema_values[ema_num];
+}
+
+double 
+StatsEMA::minEMA()
+{
+    double ret = ema_values[0];
+    for(unsigned i = 1; i<ema_values.size(); ++i) {
+	if (ema_values[i] < ret) 
+	    ret = ema_values[i];
+    }
+    return ret;
+}
+
+double 
+StatsEMA::maxEMA()
+{
+    double ret = ema_values[0];
+    for(unsigned i = 1; i<ema_values.size(); ++i) {
+	if (ema_values[i] > ret) 
+	    ret = ema_values[i];
+    }
+    return ret;
+}
+
+void
+StatsEMA::init(const std::vector<double> &_decay_rates)
+{
+    AssertAlways(_decay_rates.size() > 0,
+		 ("no decay rates specified?!"));
+    decay_rates = _decay_rates;
+    for(unsigned i = 0; i<decay_rates.size(); ++i) {
+	AssertAlways(decay_rates[i] > 0 && decay_rates[i] < 1,
+		     ("invalid decay rate %.9g specified",
+		      decay_rates[i]));
+    }
+    ema_values.resize(decay_rates.size(),0);
+    count_low.resize(decay_rates.size(),true);
+}
