e706ab8ee96c5abc76c6e3f456596e4d57fc61b6
Christopher Hoover <ch@hpl.hp.com>
2007-04-26 19:04:18 +0000

    [FIX] Disable debugInfo() glibc implementation until it works properly on all platforms.  See file for lossage.

--- src/PThread.C
+++ src/PThread.C
@@ -60,8 +60,42 @@ PThread::join()
 std::string
 PThreadMutex::debugInfo()
 {
-#if defined(_BITS_PTHREADTYPES_H) 
+#if 0 && defined(_BITS_PTHREADTYPES_H) 
     // glibc...
+
+not quite glibc...
+
+../../../../Lintel/src/PThread.C: In member function 'std::string PThreadMutex::debugInfo()':
+../../../../Lintel/src/PThread.C:66: error: 'union pthread_mutex_t' has no member named '__m_count'
+../../../../Lintel/src/PThread.C:67: error: 'union pthread_mutex_t' has no member named '__m_owner'
+../../../../Lintel/src/PThread.C:67: error: 'union pthread_mutex_t' has no member named '__m_kind'
+../../../../Lintel/src/PThread.C:68: error: 'union pthread_mutex_t' has no member named '__m_lock'
+../../../../Lintel/src/PThread.C:69: error: 'union pthread_mutex_t' has no member named '__m_lock'
+
+ch@gato:~/src/ticoli$ dpkg --search /usr/include/bits/pthreadtypes.h
+libc6-dev: /usr/include/bits/pthreadtypes.h
+ch@gato:~/src/ticoli$ dpkg --status libc6-dev
+Package: libc6-dev
+Status: install ok installed
+Priority: standard
+Section: libdevel
+Installed-Size: 12236
+Maintainer: Ubuntu Core developers <ubuntu-devel-discuss@lists.ubuntu.com>
+Architecture: i386
+Source: glibc
+Version: 2.5-0ubuntu14
+Replaces: man-db (<= 2.3.10-41), gettext (<= 0.10.26-1), ppp (<= 2.2.0f-24), libgdbmg1-dev (<= 1.7.3-24), ldso (<= 1.9.11-9), netkit-rpc, netbase (<< 4.0), kerberos4kth-dev (<< 1.2.2-10), libc6-prof (<< 2.3.5-2)
+Provides: libc-dev
+Depends: libc6 (= 2.5-0ubuntu14), linux-libc-dev
+Recommends: gcc | c-compiler
+Suggests: glibc-doc, manpages-dev
+Conflicts: libstdc++2.10-dev (<< 1:2.95.2-15), gcc-2.95 (<< 1:2.95.3-9), libpthread0-dev, libdl1-dev, libdb1-dev, libgdbm1-dev, libc6-dev (<< 2.0.110-1), locales (<< 2.1.3-5), libstdc++2.9-dev, netkit-rpc, libc-dev
+Description: GNU C Library: Development Libraries and Header Files
+ Contains the symlinks, headers, and object files needed to compile
+ and link programs which use the standard C library.
+Original-Maintainer: GNU Libc Maintainers <debian-glibc@lists.debian.org>
+ch@gato:~/src/ticoli$ 
+
     return (boost::format("mutex %p: recursive-depth %d owner %p kind %d status %d spinlock %d") 
 	    % reinterpret_cast<void *>(&m) % m.__m_count 
 	    % reinterpret_cast<void *>(m.__m_owner) % m.__m_kind 
