4715c4985e858991e49c2cb5666862a945fd07ed
Christopher Hoover <ch@hpl.hp.com>
2007-04-27 20:03:41 +0000

    * Provide a mechanism to distinguish between old and new glibc.  This   may not be fine-grained enough, but it suits us for now.
    * Select old glibc PThreadMutex::debugInfo() implementation, when
      appropriate.

--- src/PThread.C
+++ src/PThread.C
@@ -11,6 +11,18 @@
 #include <PThread.H>
 #include <signal.h>
 
+// Platform discrimination
+//
+// GLIBC_PTHREADS is presumed sufficient to always identify glibc
+//
+// GLIB_OLD_PTHREADS is not known to be sufficent to detect where the
+// old code below works and doesn't, but it is good enough for the few
+// platforms tested.
+
+#define GLIBC_PTHREADS (defined(_BITS_PTHREADTYPES_H))
+#define GLIBC_OLD_PTHREADS  (!defined(__SIZEOF_PTHREAD_MUTEX_T))
+
+
 PThread::PThread()
 {
     int ret = pthread_attr_init(&attr);
@@ -57,54 +69,36 @@ PThread::join()
     return retval;
 }
 
+#if GLIBC_PTHREADS
+#define PTHREADMUTEX_PLATFORM_DEBUG_INFO_DEFINED 1
 std::string
 PThreadMutex::debugInfo()
 {
-#if 0 && defined(_BITS_PTHREADTYPES_H) 
-    // glibc...
-
-not quite glibc...
-
-../../../../Lintel/src/PThread.C: In member function 'std::string PThreadMutex::debugInfo()':
-../../../../Lintel/src/PThread.C:66: error: 'union pthread_mutex_t' has no member named '__m_count'
-../../../../Lintel/src/PThread.C:67: error: 'union pthread_mutex_t' has no member named '__m_owner'
-../../../../Lintel/src/PThread.C:67: error: 'union pthread_mutex_t' has no member named '__m_kind'
-../../../../Lintel/src/PThread.C:68: error: 'union pthread_mutex_t' has no member named '__m_lock'
-../../../../Lintel/src/PThread.C:69: error: 'union pthread_mutex_t' has no member named '__m_lock'
-
-ch@gato:~/src/ticoli$ dpkg --search /usr/include/bits/pthreadtypes.h
-libc6-dev: /usr/include/bits/pthreadtypes.h
-ch@gato:~/src/ticoli$ dpkg --status libc6-dev
-Package: libc6-dev
-Status: install ok installed
-Priority: standard
-Section: libdevel
-Installed-Size: 12236
-Maintainer: Ubuntu Core developers <ubuntu-devel-discuss@lists.ubuntu.com>
-Architecture: i386
-Source: glibc
-Version: 2.5-0ubuntu14
-Replaces: man-db (<= 2.3.10-41), gettext (<= 0.10.26-1), ppp (<= 2.2.0f-24), libgdbmg1-dev (<= 1.7.3-24), ldso (<= 1.9.11-9), netkit-rpc, netbase (<< 4.0), kerberos4kth-dev (<< 1.2.2-10), libc6-prof (<< 2.3.5-2)
-Provides: libc-dev
-Depends: libc6 (= 2.5-0ubuntu14), linux-libc-dev
-Recommends: gcc | c-compiler
-Suggests: glibc-doc, manpages-dev
-Conflicts: libstdc++2.10-dev (<< 1:2.95.2-15), gcc-2.95 (<< 1:2.95.3-9), libpthread0-dev, libdl1-dev, libdb1-dev, libgdbm1-dev, libc6-dev (<< 2.0.110-1), locales (<< 2.1.3-5), libstdc++2.9-dev, netkit-rpc, libc-dev
-Description: GNU C Library: Development Libraries and Header Files
- Contains the symlinks, headers, and object files needed to compile
- and link programs which use the standard C library.
-Original-Maintainer: GNU Libc Maintainers <debian-glibc@lists.debian.org>
-ch@gato:~/src/ticoli$ 
-
+#if GLIBC_OLD_PTHREADS
+    // this is for some version of glibc prior to 2.5 (but maybe earlier)
+    // code by ea
     return (boost::format("mutex %p: recursive-depth %d owner %p kind %d status %d spinlock %d") 
 	    % reinterpret_cast<void *>(&m) % m.__m_count 
 	    % reinterpret_cast<void *>(m.__m_owner) % m.__m_kind 
 	    % m.__m_lock.__status 
 	    % static_cast<uint32_t>(m.__m_lock.__spinlock)).str();
 #else
-    return "no debugging information available";
+    // nothing yet for glibc 2.5 (but maybe earlier), but please
+    // implement.
+    return "no debugging information available yet (glibc 2.5)";
 #endif
 }
+#endif
+
+// fallback definition
+#if !defined(PTHREADMUTEX_PLATFORM_DEBUG_INFO_DEFINED)
+std::string
+PThreadMutex::debugInfo()
+{
+    return "no debugging information available on this platform";
+}
+#endif
+
 
 pthread_t
 PThreadNoSignals::start()
