7626eda66565c3087cc502203ebe4340db5ca6c0
Eric Anderson <eric.anderson4@hp.com>
2007-09-19 22:09:54 +0000

    Deque.[CH]: stop testing cur_pos >= 0, it is unsigned now.  Switch all   of the Assert* bits over to INVARIANT/DEBUG_INVARIANT.

--- src/Deque.C
+++ src/Deque.C
@@ -20,50 +20,53 @@ DequeTest()
 	testBad.push_back(i);
     }
     for(int i=0;i<512;i++) {
-	AssertAlways(testBad.empty() == false && testBad.front() == i,
-		     ("simple test failed?!\n"));
+	INVARIANT(testBad.empty() == false && testBad.front() == i,
+		  "simple test failed?!");
 	testBad.pop_front();
     }
 
     // test empty->size1->empty
     for(int size1=1;size1<2000;size1++) {
-	AssertAlways(testq.empty()==true,("internal\n"));
+	INVARIANT(testq.empty()==true,"internal");
 	for(int j=0;j<size1;j++) {
 	    testq.push_back(j);
-	    AssertAlways(testq.empty()==false,("internal\n"));
+	    INVARIANT(testq.empty()==false,"internal");
 	}
 	for(int j=0;j<size1;j++) {
-	    AssertAlways(testq.empty()==false,("internal\n"));
-	    AssertAlways(testq.front() == j,
-			 ("Internal Error: %d/%d\n",size1,j));
+	    INVARIANT(testq.empty()==false,"internal");
+	    INVARIANT(testq.front() == j,
+		      boost::format("Internal Error: %d/%d")
+		      % size1 % j);
 	    testq.pop_front();
 	}
-	AssertAlways(testq.empty()==true,("internal\n"));
+	INVARIANT(testq.empty()==true,"internal");
     }
     printf("pass empty->full->empty tests\n");
     // test empty->size1, size2 add/remove, ->empty
     for(int size1=1;size1<150;size1++) {
 	for(int size2=1;size2<400;size2++) {
-	    AssertAlways(testq.empty()==true,("internal\n"));
+	    INVARIANT(testq.empty()==true,"internal");
 	    for(int j=0;j<size1;j++) {
 		testq.push_back(j);
-		AssertAlways(testq.empty()==false,("internal\n"));
+		INVARIANT(testq.empty()==false,"internal");
 	    }
 	    for(int j=0;j<size2;j++) {
-		AssertAlways(testq.empty()==false,("internal\n"));
-		AssertAlways(testq.front() == j,
-			     ("Internal Error: %d/%d/%d\n",size1,size2,j));
+		INVARIANT(testq.empty()==false,"internal");
+		INVARIANT(testq.front() == j,
+			  boost::format("Internal Error: %d/%d/%d")
+			  % size1 % size2 %j);
 		testq.pop_front();
 		testq.push_back((j+size1));
 	    }
 	    for(int j=size2;j<size1+size2;j++) {
-		AssertAlways(testq.empty()==false,("internal\n"));
+		INVARIANT(testq.empty()==false,"internal");
 		int t = testq.front();
 		testq.pop_front();
-		AssertAlways(t == j,
-			     ("Internal Error: %d/%d/%d/%d\n",t,size1,size2,j));
+		INVARIANT(t == j,
+			  boost::format("Internal Error: %d/%d/%d/%d")
+			  % t % size1 % size2 % j);
 	    }
-	    AssertAlways(testq.empty()==true,("internal\n"));
+	    INVARIANT(testq.empty()==true,"internal");
 	}
     }
     printf("pass partial fill tests\n");
