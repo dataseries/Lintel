3fd6296222d3e17ae1932d9de26feae0a0f5b1dc
Eric Anderson <eric.anderson4@hp.com>
2008-05-12 07:34:05 +0000

    * Bugfixes and extentions to get DataSeries building
    CMakeConfig.txt: Provide option to build static libraries; move the
      PERL substitutions into LintelCMakeUtil.cmake
    
    CMakeLists.txt: check that we have the minimum cmake version
    
    cmake-modules/CMakeLists.txt: add FindCrypto, FindLZO, LintelFindLibrary
      cmake modules.
    
    FindLintel.cmake: simplify to use the LintelFindLibrary macro
    
    LintelCMakeUtil.cmake: remove the configure message, reformat to 4 character
      indent, add macro for finding a perl module (maybe big enough to separate
      out)
    
    include/CMakeLists.txt: remove minimum version test (should have been in
      parent) add TODO.
    
    BatchParallel/CMakeLists.txt: fix destination for files
    
    src/CMakeLists.txt: support static build, properly link with thread 
      library in LintelPThread

--- cmake-modules/FindLintel.cmake
+++ cmake-modules/FindLintel.cmake
@@ -1,56 +1,27 @@
-# Find the Lintel includes and library
 #
-#  LINTEL_INCLUDE_DIR - where to find Lintel/LintelAssert.H, ...
-#  LINTEL_LIBRARIES   - List of libraries when using Lintel
-#  LINTEL_FOUND       - True if Lintel found.
+# (c) Copyright 2008, Hewlett-Packard Development Company, LP
+#
+#  See the file named COPYING for license details
+#
+# All of the options and dependencies for the various cmake sub-bits
+#
+# Find the Lintel, LintelPThread includes and library
+#
+#  set LINTEL_FIND_REQUIRED to require the LINTEL library
+#  otherwise, the user will be given a WITH_LINTEL option.
+#
+#  set LINTELPTHREAD_FIND_REQUIRED to require the LINTELPTHREAD library
+#  otherwise, the user will be given a WITH_LINTELPTHREAD option.
+#
+#  LINTEL_INCLUDE_DIR - where to find openssl/sha.h
+#  LINTEL_LIBRARIES   - List of libraries when using LINTEL
+#  LINTEL_ENABLED     - True if LINTEL is enabled
+# 
+#  LINTELPTHREAD_INCLUDE_DIR - where to find openssl/sha.h
+#  LINTELPTHREAD_LIBRARIES   - List of libraries when using LINTELPTHREAD
+#  LINTELPTHREAD_ENABLED     - True if LINTELPTHREAD is enabled
 
-IF (LINTEL_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(LINTEL_FIND_QUIETLY TRUE)
-ENDIF (LINTEL_INCLUDE_DIR)
+INCLUDE(LintelFindLibrary)
 
-# This is the recommended cmake idiom to use a locally built version
-# of a library in preference to the system one.
-
-FIND_PATH(LINTEL_INCLUDE_DIR Lintel/LintelAssert.H
-  ${CMAKE_INSTALL_PREFIX}/include
-  NO_DEFAULT_PATH
-)
-
-FIND_PATH(LINTEL_INCLUDE_DIR Lintel/LintelAssert.H)
-
-# TODO: figure out why in some cases this complains that libLintel.so
-# can't have an absolute path, and in others it has no problem.
-
-FIND_LIBRARY(LINTEL_LIBRARY 
-  NAMES Lintel 
-  PATHS ${CMAKE_INSTALL_PREFIX}/lib
-  NO_DEFAULT_PATH)
-
-FIND_LIBRARY(LINTEL_LIBRARY NAMES Lintel)
-
-IF (LINTEL_INCLUDE_DIR AND LINTEL_LIBRARY)
-   SET(LINTEL_FOUND TRUE)
-   SET(LINTEL_LIBRARIES ${LINTEL_LIBRARY})
-ELSE (LINTEL_INCLUDE_DIR AND LINTEL_LIBRARY)
-   SET(LINTEL_FOUND FALSE)
-   SET(LINTEL_LIBRARIES)
-ENDIF (LINTEL_INCLUDE_DIR AND LINTEL_LIBRARY)
-
-IF (LINTEL_FOUND)
-   IF (NOT LINTEL_FIND_QUIETLY)
-      MESSAGE(STATUS "Found Lintel: ${LINTEL_LIBRARY}")
-   ENDIF (NOT LINTEL_FIND_QUIETLY)
-ELSE (LINTEL_FOUND)
-   IF (LINTEL_FIND_REQUIRED)
-      MESSAGE(STATUS "Looked for Lintel libraries named Lintel in ${CMAKE_INSTALL_PREFIX}/lib and system paths")
-      MESSAGE(STATUS "got: LINTEL_INCLUDE_DIR=${LINTEL_INCLUDE_DIR}")
-      MESSAGE(STATUS "got: LINTEL_LIBRARY=${LINTEL_LIBRARY}")
-      MESSAGE(FATAL_ERROR "ERROR: Could NOT find Lintel library")
-   ENDIF (LINTEL_FIND_REQUIRED)
-ENDIF (LINTEL_FOUND)
-
-MARK_AS_ADVANCED(
-  LINTEL_LIBRARY
-  LINTEL_INCLUDE_DIR
-)
+LINTEL_FIND_LIBRARY_CMAKE_INCLUDE_FILE(LINTEL Lintel/LintelAssert.hpp Lintel)
+LINTEL_FIND_LIBRARY_CMAKE_INCLUDE_FILE(LINTELPTHREAD Lintel/PThread.hpp LintelPThread)
--- cmake-modules/LintelCMakeUtil.cmake
+++ cmake-modules/LintelCMakeUtil.cmake
@@ -1,32 +1,82 @@
 MACRO(LINTEL_CONFIG_FILE file_name)
-  MESSAGE("${file_name}: substitute @vars")
-  CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/${file_name}.in
-                 ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
-                 @ONLY)
+#    MESSAGE("${file_name}: substitute @vars")
+    CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/${file_name}.in
+                   ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
+                   @ONLY)
 ENDMACRO(LINTEL_CONFIG_FILE)
 
 MACRO(LINTEL_INSTALL_CONFIG_PROGRAM file_name)
-  LINTEL_CONFIG_FILE(${file_name})				   
-  INSTALL(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
-          DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
+    LINTEL_CONFIG_FILE(${file_name})				   
+    INSTALL(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
+            DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
 ENDMACRO(LINTEL_INSTALL_CONFIG_PROGRAM)
 
 MACRO(LINTEL_INSTALL_CONFIG_FILE file_name dest_prefix)
-  LINTEL_CONFIG_FILE(${file_name})
-  INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
-          DESTINATION ${dest_prefix})
+    LINTEL_CONFIG_FILE(${file_name})
+    INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${file_name}
+            DESTINATION ${dest_prefix})
 ENDMACRO(LINTEL_INSTALL_CONFIG_FILE)
 
 MACRO(LINTEL_INSTALL_ONE_FILE_PATH path_name dest_prefix)
-  GET_FILENAME_COMPONENT(tmp ${path_name} PATH)
-  INSTALL(FILES ${path_name}
-	  DESTINATION ${dest_prefix}/${tmp})
+    GET_FILENAME_COMPONENT(tmp ${path_name} PATH)
+    INSTALL(FILES ${path_name}
+  	  DESTINATION ${dest_prefix}/${tmp})
 ENDMACRO(LINTEL_INSTALL_ONE_FILE_PATH)
 
 # prefix file ...
 MACRO(LINTEL_INSTALL_FILE_PATH dest_prefix)
-  FOREACH(file ${ARGN})
-    LINTEL_INSTALL_ONE_FILE_PATH(${file} ${dest_prefix})
-  ENDFOREACH(file)
+    FOREACH(file ${ARGN})
+        LINTEL_INSTALL_ONE_FILE_PATH(${file} ${dest_prefix})
+    ENDFOREACH(file)
 ENDMACRO(LINTEL_INSTALL_FILE_PATH)
+
+# This macro searches for the perl module ${module_name}. It sets
+# ${variable}_FOUND to whether the module was found.  It creates a
+# configuration variable WITH_${variable} so the user can control
+# whether this perl module should be used.  Finally in combines these
+# into ${variable}_ENABLED for use in the CMakeLists.txt to determine
+# if we should build using this module.
+
+MACRO(LINTEL_FIND_PERL_MODULE module_name variable)
+    IF("${PERL_FOUND}" STREQUAL "")
+        INCLUDE(FindPerl)
+    ENDIF("${PERL_FOUND}" STREQUAL "")
+
+    IF("${${variable}_FOUND}" STREQUAL "YES")
+        # ... nothing to do, already found it
+    ELSEIF("${WITH_${variable}}" STREQUAL "OFF")
+        # user has explicitly disabled it; don't bother to check
+    ELSE("${${variable}_FOUND}" STREQUAL "YES")
+         SET(WITH_${variable} "ON" CACHE BOOL "Enable use of the ${module_name} perl module")
+         SET(LFPM_found NO)
+         IF(PERL_FOUND)
+             EXEC_PROGRAM(${PERL_EXECUTABLE}
+                          ARGS -e "\"use lib '${CMAKE_INSTALL_PREFIX}/share/perl5'; use ${module_name};\""
+                          RETURN_VALUE LFPM_return_value)
+             IF("${LFPM_return_value}" STREQUAL 0)
+                 SET(LFPM_found YES)
+             ENDIF("${LFPM_return_value}" STREQUAL 0)
+         ENDIF(PERL_FOUND)
+         SET(${variable}_FOUND ${LFPM_found} CACHE BOOL "Found ${module_name} perl module" FORCE)
+         MARK_AS_ADVANCED(${variable}_FOUND)
+    ENDIF("${${variable}_FOUND}" STREQUAL "YES")
+
+    IF("${WITH_${variable}}" STREQUAL ON AND ${${variable}_FOUND} STREQUAL "YES")
+        SET(${variable}_ENABLED YES)
+    ELSE("${WITH_${variable}}" STREQUAL ON AND ${${variable}_FOUND} STREQUAL "YES")
+        SET(${variable}_ENABLED NO)
+    ENDIF("${WITH_${variable}}" STREQUAL ON AND ${${variable}_FOUND} STREQUAL "YES")
+
+#    MESSAGE("HIYA ${variable} ${${variable}_ENABLED}")
+ENDMACRO(LINTEL_FIND_PERL_MODULE)
+
+# need to do this to pick up user installed modules.
+# need unshift so that if we have commonly installed perl modules
+# and user installed ones that the user installed ones take precedence.
+SET(PERL_MODULES_INC_UNSHIFT "BEGIN { unshift(@INC,'${CMAKE_INSTALL_PREFIX}/share/perl5') if grep(\$_ eq '${CMAKE_INSTALL_PREFIX}/share/perl5', @INC) == 0;};")
+# need this so that we can regression test without having to install first.
+# and can't do it through the standard PERLLIB env variable as that
+# would get overridden by the earlier unshift.
+# TODO: check to see if setting PERL5LIB with PERL_SHAREDIR and this one works now.
+SET(PERL_MODULES_INC_UNSHIFT "${PERL_MODULES_INC_UNSHIFT} BEGIN { unshift(@INC, \$ENV{LINTEL_REGRESSION_TEST_INC_DIR}) if defined \$ENV{LINTEL_REGRESSION_TEST_INC_DIR}; };")
 
