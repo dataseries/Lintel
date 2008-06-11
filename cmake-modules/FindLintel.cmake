c632eee27eb064d5151202d2ade0cd678155466a
Eric Anderson <eric.anderson4@hp.com>
2008-06-11 22:32:11 +0000

    FindLintel.cmake: search for Lintel/AssertBoost.hpp, fix documentation
    LintelFind.cmake: extend to support extra paths for includes searching
      (for thrift).  Unify the variables so that we set variable_includes and
      variable_libraries for everything you need and variable_include_dir and
      variable_library for the library specific location.

--- cmake-modules/FindLintel.cmake
+++ cmake-modules/FindLintel.cmake
@@ -13,15 +13,15 @@
 #  set LINTELPTHREAD_FIND_REQUIRED to require the LINTELPTHREAD library
 #  otherwise, the user will be given a WITH_LINTELPTHREAD option.
 #
-#  LINTEL_INCLUDE_DIR - where to find openssl/sha.h
+#  LINTEL_INCLUDE_DIR - where to find Lintel/AssertBoost.hpp
 #  LINTEL_LIBRARIES   - List of libraries when using LINTEL
 #  LINTEL_ENABLED     - True if LINTEL is enabled
 # 
-#  LINTELPTHREAD_INCLUDE_DIR - where to find openssl/sha.h
+#  LINTELPTHREAD_INCLUDE_DIR - where to find Lintel/PThread.hpp
 #  LINTELPTHREAD_LIBRARIES   - List of libraries when using LINTELPTHREAD
 #  LINTELPTHREAD_ENABLED     - True if LINTELPTHREAD is enabled
 
 INCLUDE(LintelFind)
 
-LINTEL_FIND_LIBRARY_CMAKE_INCLUDE_FILE(LINTEL Lintel/LintelAssert.hpp Lintel)
+LINTEL_FIND_LIBRARY_CMAKE_INCLUDE_FILE(LINTEL Lintel/AssertBoost.hpp Lintel)
 LINTEL_FIND_LIBRARY_CMAKE_INCLUDE_FILE(LINTELPTHREAD Lintel/PThread.hpp LintelPThread)
