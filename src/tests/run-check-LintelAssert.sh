2db53a42ce532d34a063e0e4f097e70a7b3e26c4
Christopher Hoover <ch@hpl.hp.com>
2008-08-05 20:34:56 +0000

    run-check-LintelAssert.sh: grep -v '^Aborted$' on the output to avoid a false failure detection due only to shell differences.

--- src/tests/run-check-LintelAssert.sh
+++ src/tests/run-check-LintelAssert.sh
@@ -15,7 +15,7 @@ echo "==============================================================="
 echo ">>>>Expect Assert to fail on fatal fault"
 echo LintelAssert -F
 ./check-LintelAssert -F
-) 2>&1 | grep -v './run-check-LintelAssert.sh' | sed "s/.cpp, [0-9]*:/.cpp, LINENO:/" | sed 's,Assert failure in .*/tests/check-Lintel,Assert failure in tests/check-Lintel,' >./tmp-LintelAssert.out
+) 2>&1 | grep -v './run-check-LintelAssert.sh' | grep -v 'Aborted$' | sed "s/.cpp, [0-9]*:/.cpp, LINENO:/" | sed 's,Assert failure in .*/tests/check-Lintel,Assert failure in tests/check-Lintel,' >./tmp-LintelAssert.out
 set -e
 echo "diff -c $1/./LintelAssert.good ./tmp-LintelAssert.out"
 diff -c $1/./LintelAssert.good ./tmp-LintelAssert.out
