9f15db5a5df8eb1b4c3dcf5fc98c6ce498d259f5
Eric Anderson <eric.anderson4@hp.com>
2008-09-25 19:28:59 +0000

    * Begin cleanup on the work done during the FAST paper; this adds in      the Tuples and HashTupleStats stuff, but doesn't do the testing of it      because there was a lot of work in making the hash functions do the      right thing, which in this case is defined as not having to specify      both the T::operator () and the hash(T) variants of the same function.      That works and has a bunch of tests.
    HashFns.hpp: the new header for hash functions, along with the magic to let
      it auto-select between various possible implementations.  A much simpler
      and more general auto-selection than the previous HashMap.hpp implementation.
    
    HashTupleStats.hpp: the header for hash-map tuple stats that forms the 
      basis of the cube stuff.
    
    Tuples.hpp: extentions to boost::tuples to do partial tuples and the like.
    
    HashFns.cpp: move the BobJenkins stuff into here
    
    hashfns.cpp: testing for HashFns
    
    tuples.cpp: testing for Tuples (disabled)
    
    COPYING: add another copyright callout for the BobJenkins code; it didn't 
      have the copyright phrase, so was missed in the first round.
    
    include/Lintel/CMakeLists.txt: install the new headers
    
    HashMap.hpp: remove most of the code dealing with HashFns from here,
       make the HashMap_hash structure depend on the lintel::Hash structure
       so everything still works.  Eventually need to deprecate HashMap_hash.
    
    HashTable.hpp: Remove BobJenkins stuff, add various inline functions/
      #defines to make the namespaces the same; need to deprecate the global
      namespace bits.
    
    src/CMakeLists.txt: add HashFns.cpp
    
    HashTable.cpp: remove all the BobJenkins stuff, it went into HashFns.cpp
    
    src/tests/CMakeLists.txt: add hashfns/tuples tests.

---
+++ src/tests/hashfns.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+#include <Lintel/AssertBoost.hpp>
+#include <Lintel/HashFns.hpp>
+
+using lintel::hash;
+using lintel::Hash;
+using boost::format;
+using namespace std;
+
+template<typename T> void check(const T &v, uint32_t hv) {
+    INVARIANT(hash(v) == hv, format("bad hash(%s): %d != %d") % v % hash(v) % hv);
+
+    Hash<T> tmp;
+    INVARIANT(tmp(v) == hv, format("bad Hash(%s): %d != %d") % v % hash(v) % hv);
+}
+
+namespace foo {
+    struct TestNoHash {
+	TestNoHash() : x(0), y(0) { }
+	TestNoHash(int a, int b) : x(a), y(b) { }
+	int x, y;
+    };
+
+    ostream &operator <<(ostream &to, const foo::TestNoHash &a) {
+	to << format("tnh(%d,%d)") % a.x % a.y;
+	return to;
+    }
+
+    uint32_t hashType(const foo::TestNoHash &a) { // this can also be in namespace lintel
+	return a.x ^ a.y;
+    }
+}
+
+double xhash(const foo::TestNoHash &a) {
+    return a.x ^ a.y;
+}
+
+struct TestHash {
+    TestHash() : x(0), y(0) { }
+    TestHash(int a, int b) : x(a), y(b) { }
+    int x, y;
+
+    uint32_t hash() const { 
+	return x ^ y;
+    }
+};
+
+ostream &operator <<(ostream &to, const TestHash &a) {
+    to << format("th(%d,%d)") % a.x % a.y;
+    return to;
+}
+
+int main() {
+    bool a = true;
+    char b = 'a';
+    unsigned char c = 'x';
+    int16_t d = 1234;
+    uint16_t e = 53312;
+    int32_t f = 777;
+    uint32_t g = 3838383;
+    int64_t h = 13567876319LL;
+    uint64_t i = 45678976511ULL;
+    short j = -32156;
+    int k = 4111;
+    long l = 313515111;
+    long long m = 151351351351515ULL;
+    const string n("foo");
+    foo::TestNoHash o(15,127);
+    TestHash p(2048,1);
+
+    check(a, 1);
+    check(b, 97);
+    check(c, 120);
+    check(d, 1234);
+    check(e, 53312);
+    check(f, 777);
+    check(g, 3838383);
+    check(h, 3487493002UL);
+    check(i, 1129882560UL);
+    check(j, 33380);
+    check(k, 4111);
+    check(l, 313515111);
+    check(m, 4217888175U);
+    check(n, 2039183822U);
+    check(o, 15 ^ 127);
+    check(p, 2049);
+
+    SINVARIANT(!lintel::detail::HasMemberHash<foo::TestNoHash>::value); 
+    SINVARIANT(lintel::detail::HasMemberHash<TestHash>::value);
+
+    BOOST_STATIC_ASSERT(sizeof(xhash(o)) != 4);
+    cout << "passed hashfns tests\n";
+
+    return 0;
+}
+
---
+++ src/tests/tuples.cpp
@@ -0,0 +1,23 @@
+// #include <Lintel/HashFns.hpp>
+// #include <Lintel/Tuples.hpp>
+// 
+// using namespace std;
+// using namespace lintel::tuples;
+// using boost::format;
+// 
+// typedef boost::tuple<bool, char, int32_t, int64_t, string> Tuple;
+// 
+// int main() {
+//     int32_t a = 5;
+// 
+//     uint32_t v = lintel::hashfns::Hash<int32_t>()(a);
+//     Tuple tuple1(true, 'a', 7, 123456789023LL, "Hello, World");
+//     // alternate so attributes are <, >, <, >, <
+//     Tuple tuple2(false, 'b', 6, 123456789022LL, "Iello, World");
+// 
+// //    INVARIANT(hash(tuple1) == 33, format("got %d?") % hash(tuple1));
+// //    INVARIANT(hash(tuple2) == 33, format("got %d?") % hash(tuple2));
+//     return 0;
+// }
+
+int main() { }
