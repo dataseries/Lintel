41556c371902d1eacbee4b98d8128333f1ceef4f
Eric Anderson <eric.anderson4@hp.com>
2008-10-27 07:15:37 +0000

    * Fix HashTupleStats.hpp, add regression tests.
    HashTupleStats.hpp: add the zeroCubeBaseCount, zeroWalk functions in, they 
      are needed for HTS.zeroWalk()  Fix some glitches in the documentation; 
      make fillHashUniqueTuple const so it can be called from walkZeroes.
    
    hashtuplestats.cpp: regression tests for the various functions in 
      HashTupleStats.

--- include/Lintel/HashTupleStats.hpp
+++ include/Lintel/HashTupleStats.hpp
@@ -17,6 +17,11 @@
 #include <Lintel/Stats.hpp>
 #include <Lintel/Tuples.hpp>
 
+// TODO: consider renaming this to TupleHashMap or something like that
+// once we support a copy constructor in StatsQuantile, and/or come up
+// with some way to use a ptr argument and have a function that does
+// the initialization.
+
 namespace lintel {
     class StatsCubeFns {
     public:
@@ -64,6 +69,45 @@ namespace lintel {
 	    hut.get_head().add(v.get_head());
 	    hashUniqueTupleAdd(hut.get_tail(), v.get_tail());
 	}
+
+	double zeroCubeBaseCount(const boost::tuples::null_type) { 
+	    return 1; 
+	}
+
+	template<class HUT>
+	double zeroCubeBaseCount(const HUT &hut) {
+	    return hut.get_head().size() * zeroCubeBaseCount(hut.get_tail());
+	}
+
+	template<class KeyBase, class BaseData, class Function>
+	void zeroWalk(const boost::tuples::null_type, const boost::tuples::null_type, 
+		      KeyBase &key_base, const BaseData &base_data, Stats &null_stat, 
+		      const Function &fn) {
+	    typedef typename BaseData::const_iterator iterator;
+	    iterator i = base_data.find(key_base);
+	    if (i == base_data.end()) {
+		fn(key_base, null_stat); 
+	    } else {
+		fn(key_base, *(i->second)); 
+	    }
+	}
+	
+	template<class HUT, class KeyTail, class KeyBase, class BaseData, 
+		 class Function>
+	void zeroWalk(const HUT &hut, KeyTail &key_tail, KeyBase &key_base, 
+		      const BaseData &base_data, Stats &null_stat, 
+		      const Function &fn) {
+	    typedef typename HUT::head_type::const_iterator const_iterator;
+	    
+	    for(const_iterator i = hut.get_head().begin(); 
+		i != hut.get_head().end(); ++i) {
+		key_tail.get_head() = *i;
+		
+		zeroWalk(hut.get_tail(), key_tail.get_tail(), key_base,
+			 base_data, null_stat, fn);
+	    }
+	}
+
     }
 
     /// A hash table from tuples to "stats" Includes a bunch of
@@ -101,7 +145,7 @@ namespace lintel {
 	    getHashEntry(key).add(value);
 	}
 
-	/// apply walk_fn(const Tuple &, StatsT *) to each entry in the
+	/// apply walk_fn(const Tuple &, StatsT &) to each entry in the
 	/// hash table; entries are walked in a pseudo-random order.
 	void walk(const WalkFn &walk_fn) const {
 	    for(HTSconst_iterator i = data.begin(); i != data.end(); ++i) {
@@ -109,7 +153,7 @@ namespace lintel {
 	    }
 	}
 
-	/// apply walk_fn(const Tuple &, StatsT *) to each entry,
+	/// apply walk_fn(const Tuple &, StatsT &) to each entry,
 	/// sorted in tuple order.  If you can avoid using this
 	/// function, do, it will use additional memory as a result of
 	/// having to sort the tuples.
@@ -128,7 +172,7 @@ namespace lintel {
 	    }
 	}
 
-	/// apply walk_fn(const Tuple &, StatsT *) to each entry,
+	/// apply walk_fn(const Tuple &, StatsT &) to each entry,
 	/// including ones that would have been present if the full
 	/// cross product of all tuple values were present.  For
 	/// example, if there are two entries in the HTS ([0,1] -> v,
@@ -146,8 +190,8 @@ namespace lintel {
 	/// build a tuple of hash uniques out of the entries in the
 	/// table, useful for determining all of the values that are
 	/// used for any of the entries.
-	void fillHashUniqueTuple(HashUniqueTuple &hut) {
-	    for(HTSiterator i = data.begin(); i != data.end(); ++i) {
+	void fillHashUniqueTuple(HashUniqueTuple &hut) const {
+	    for(HTSconst_iterator i = data.begin(); i != data.end(); ++i) {
 		hashUniqueTupleAdd(hut, i->first);
 	    }
 	}
@@ -170,7 +214,7 @@ namespace lintel {
 	    StatsT *zero = stats_factory_fn();
 
 	    Tuple tmp_key;
-	    zeroWalk(hut, tmp_key, tmp_key, data, *zero, walk_fn);
+	    detail::zeroWalk(hut, tmp_key, tmp_key, data, *zero, walk_fn);
 	
 	    delete zero;
 	}
@@ -225,8 +269,8 @@ namespace lintel {
 	size_t memoryUsage() const {
 	    return data.memoryUsage() + data.size() * sizeof(StatsT) + sizeof(*this);
 	}
-    private:
 
+    private:
 	HTSMap data;
 	StatsFactoryFn stats_factory_fn;
     };
--- src/tests/hashtuplestats.cpp
+++ src/tests/hashtuplestats.cpp
@@ -4,18 +4,105 @@ using namespace std;
 
 typedef boost::tuple<int32_t, bool, string> IBSTuple;
 
-int main() {
-#if 0
-    IBSTuple tmp(1,true,"a");
 
-    return lintel::tuples::hash(tmp);
+static bool vw_seen[10];
+
+void verifyWalk(const IBSTuple &tuple, Stats &stat) {
+    int32_t v = tuple.get<0>();
+    SINVARIANT(v >= 0 && v < 10);
+    SINVARIANT(!vw_seen[v]);
+    SINVARIANT(tuple.get<1>() == ((v % 2) == 0));
+    SINVARIANT(tuple.get<2>() == str(boost::format("%d") % (v * 10)));
+    SINVARIANT(stat.count() == 1);
+    SINVARIANT(stat.mean() == v*5);
+    vw_seen[v] = true;
+}
+
+static int32_t prev = -1;
+void verifySortedWalk(const IBSTuple &tuple, Stats &stat) {
+    ++prev;
+    int32_t v = tuple.get<0>();
+    SINVARIANT(v == prev);
+    verifyWalk(tuple, stat);
+}
+
+void verifyZeroWalk(const IBSTuple &tuple, Stats &stat) {
+    if (stat.count() == 1) {
+	verifyWalk(tuple, stat);
+    } else {
+	SINVARIANT(stat.count() == 0);
+	int32_t v = tuple.get<0>();
+	SINVARIANT(v >= 0 && v < 10);
+	SINVARIANT(tuple.get<1>() != ((v % 2) == 0) ||
+		   tuple.get<2>() != str(boost::format("%d") % (v * 10)));
+    }
+}
+
+bool pruneGreaterEqual5(const IBSTuple &tuple) {
+    return tuple.get<0>() >= 5;
+}
+
+int main() {
+    IBSTuple tmp(1,true,"a");
 
     lintel::HashTupleStats<IBSTuple> hts;
 
     for(int32_t i = 0; i < 10; ++i) {
-	hts.add(IBSTuple(i,(i % 2) == 0, str(boost::format("%d") % (i * 10))), 1);
+	hts.add(IBSTuple(i,(i % 2) == 0, str(boost::format("%d") % (i * 10))), i*5);
     }
-#endif
+
+    hts.walk(boost::bind(&verifyWalk, _1, _2));
+    for(unsigned i = 0; i < 10; ++i) {
+	SINVARIANT(vw_seen[i]);
+	vw_seen[i] = false;
+    }
+
+    hts.walkOrdered(boost::bind(&verifySortedWalk, _1, _2));
+
+    for(unsigned i = 0; i < 10; ++i) {
+	SINVARIANT(vw_seen[i]);
+	vw_seen[i] = false;
+    }
+
+    hts.walkZeros(boost::bind(&verifyZeroWalk, _1, _2));
+
+    for(unsigned i = 0; i < 10; ++i) {
+	SINVARIANT(vw_seen[i]);
+	vw_seen[i] = false;
+    }
+
+    lintel::HashTupleStats<IBSTuple>::HashUniqueTuple hut;
+
+    hts.fillHashUniqueTuple(hut);
+
+    SINVARIANT(hut.get<0>().size() == 10);
+    SINVARIANT(hut.get<1>().size() == 2);
+    SINVARIANT(hut.get<2>().size() == 10);
+
+    for(unsigned i = 0; i < 10; ++i) {
+	SINVARIANT(hut.get<0>().exists(i));
+	SINVARIANT(hut.get<2>().exists(str(boost::format("%d") % (i * 10))));
+    }
+
+    hts.prune(boost::bind(&pruneGreaterEqual5, _1));
     
+    hut.get<0>().clear();
+    hut.get<1>().clear();
+    hut.get<2>().clear();
+    hts.fillHashUniqueTuple(hut);
+
+    SINVARIANT(hut.get<0>().size() == 5);
+    SINVARIANT(hut.get<1>().size() == 2);
+    SINVARIANT(hut.get<2>().size() == 5);
+
+    prev = -1;
+    hts.walkOrdered(boost::bind(&verifySortedWalk, _1, _2));
+    SINVARIANT(prev == 4);
+   
+    prev = -1;
+    hts.clear();
+    hts.walkOrdered(boost::bind(&verifySortedWalk, _1, _2));
+    SINVARIANT(prev == -1);
+
     return 0;
 }
