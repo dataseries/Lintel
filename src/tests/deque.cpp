e74a790b5656bb4767cd6a2e60e0d97181256368
Eric Anderson <eric.anderson4@hp.com>
2009-01-07 01:21:34 +0000

    * TODO: test the rest of deque

--- src/tests/deque.cpp
+++ src/tests/deque.cpp
@@ -13,14 +13,38 @@
 
 #include <Lintel/Deque.hpp>
 using namespace std;
+using boost::format;
 
 void testPushBack() {
     Deque<int> deque;
 
+    SINVARIANT(deque.empty());
+    deque.reserve(8);
+    SINVARIANT(deque.empty() && deque.capacity() == 8);
     for(int i = 0; i < 5; ++i) {
 	deque.push_back(i);
+	SINVARIANT(deque.back() == i);
+	SINVARIANT(deque.size() == static_cast<size_t>(i + 1));
     }
 
+    for(int i = 0; i < 5; ++i) {
+	SINVARIANT(deque.front() == i);
+	deque.pop_front();
+	deque.push_back(i);
+	SINVARIANT(deque.back() == i);
+	SINVARIANT(deque.size() == 5);
+    }
+
+    {
+	Deque<int>::iterator i = deque.begin(); 
+	int j = 0;
+	while(i != deque.end()) {
+	    INVARIANT(*i == j, format("%d != %d") % *i % j);
+	    ++i;
+	    ++j;
+	}
+    }
+	
     vector<int> avec;
     for(int i = 5; i < 10; ++i) {
 	avec.push_back(i);
@@ -31,9 +55,9 @@ void testPushBack() {
 	SINVARIANT(deque.front() == i);
 	deque.pop_front();
     }
+    SINVARIANT(deque.empty());
 }
 
-// TODO: test the rest of deque.
 int main(int argc, char *argv[]) {
     testPushBack();
     cout << "deque tests passed.\n";
