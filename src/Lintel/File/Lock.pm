e9090251b1f6fb99bcc8b976d894b4f46db7d0bc
Kevin Smathers <ks1@hpl.hp.com>
2009-04-13 21:03:19 +0000

    File/Lock.pm: Changed to use sysopen instead of FileHandle so that the flags could be set correctly.  Added a lockFileEx() method that takes named parameters instead of positional.  Added $mode and $flags parameters to lockFile() so that the caller can control the file mode. deptool.in: Deptool dependency checking is now thorough.  Subprojects will no longer be rebuilt base on their listing order, instead they will only be rebuilt if their own dependencies indicate that they are out of date relative to the installation of a dependent project.
    MTN 
    deptool.in: The commit option now includes -t, -d, -b options.  Testing is
    off by default for commit but can be turned on with -t.

--- src/Lintel/File/Lock.pm
+++ src/Lintel/File/Lock.pm
@@ -1,7 +1,7 @@
 package Lintel::File::Lock;
 use strict;
 use warnings;
-use Fcntl ':flock';
+use Fcntl qw/:flock :DEFAULT/;
 use FileHandle;
 
 =pod
@@ -27,28 +27,56 @@ Lintel::File::Lock - locking and unlocking of files
 
 =head1 FUNCTIONS
 
-=head2 getLock(filename, waittime, verbose)
+=head2 getLock(filename, [waittime, verbose, flags, mode])
 
-getLock will attempt to acquire a lock on filename.  The file will be
-created if it doesn't exist, but will not be modified by the locking.
-It will return a locked rw FileHandle on success.  If you are going to
+   my $fh = getLock( $filename, $waittime, $verbose, $flags, $mode);
+   my $fh = getLockEx( $filename,
+		       waittime => $waittime,
+		       verbose => $verbose,
+		       flags => $flags,
+		       mode => $mode);
+
+getLock will attempt to acquire a lock on filename.  By default, the 
+file will be created if it doesn't exist, and getLock will return
+an exclusive locked read/write FileHandle on success.  If you are going to
 write to the file, you should call one of C<seek($fh, 0, SEEK_END); or
 seek($fh, 0, SEEK_SET);> to set the current writing position.
 
-To release the lock, set the filehandle to undef.  If no waittime is
-specified, it will wait forever.  Otherwise if it has waited for
-waittime seconds, it will return undef.  Various additional
-information is printed to stderr if verbose is specified.  If some
-rare errors occur it will die.
+To release the lock, set the filehandle to undef.  
+
+If no waittime is specified, it will wait forever.  Otherwise if it 
+has waited for waittime seconds, it will return undef.  Various additional 
+information is printed to stderr if verbose is specified.  
+
+The flags parameter is a bitwise or of the flags to sysopen.  The
+default is to use O_CREAT | O_RDWR.
+
+The mode parameter is used to change the access rights to the file for
+a file that is being newly created.  If the mode parameter is omitted
+it defaults to 0666.
+
+If some rare errors occur getLock will die.  Locking requests may be silently
+ignored by some filesystems, and in particular has known errors in many NFS
+implementations.  Recent Linux NFS clients talking to a recent Linux NFS server
+have functional locking.
 
 =cut
 
-sub getLock { 
-    my ($filename, $waittime, $verbose) = @_;
-    die "?" unless defined $filename;
+sub getLockEx {
+    my ($filename, %param) = @_;
+    return getLock( $filename, $param{waittime}, $param{verbose}, 
+	            $param{flags}, $param{mode});
+}
 
-    my $fh = new FileHandle "+<$filename"
-	or die "Unable to open $filename for rw: $!";
+sub getLock { 
+    my ($filename, $waittime, $verbose, $flags, $mode) = @_;
+    die "?" unless defined $filename;
+    $flags ||= O_CREAT | O_RDWR;
+    $mode ||= 0666;
+
+    my $fh;
+    sysopen( $fh, $filename, $flags, $mode) 
+	or die "Unable to create or open $filename for read/write: $!";
     if (defined $waittime) {
 	die "??" if $waittime < 0;
 	my $start = time;
