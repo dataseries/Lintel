0a22b6001e4761f10a852eb8d24c076ef3c5a03a
Eric Anderson <eric.anderson4@hp.com>
2009-07-13 22:06:30 +0000

    * Update OS version detecting to include SuSE. Slightly change what we      include in the "unknown" versions of the os entries.  Make sure we pause      for user input when we detect an unknown OS.    * Cleanup the atomic counter test to scale with machine size
    PThread.hpp.in: document return values from getNCpus()
    
    deptool.in: Replace non-word and spaces with _ when generating the "unknown"
      versions of the OS string.  Other changes per above.
    
    atomic_counter.cpp: lots of minor style fixups.  Made the test scale
      with the number of cpus on a machine -- the default of 250 threads
      and 10000 iterations takes a really long time on a slow 2-cpu
      machine, and doesn't really help with the testing.  Two threads and
      1 million iterations is much faster and is likely to catch all the
      same race conditions.  Therefore set # threads = # cpus, or if we
      don't know, guess 32.

--- src/tests/atomic_counter.cpp
+++ src/tests/atomic_counter.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <Lintel/AtomicCounter.hpp>
 #include <Lintel/PThread.hpp>
@@ -11,29 +12,27 @@ lintel::AtomicCounter correct_count(0);
 int32_t incorrect_count(0);
 
 // Spin barrier -- be wary!
-volatile int32_t doStart(0);
+static volatile int32_t doStart(0);
 
 
 class AtomicCounterThread : public PThread {
 public:
-    AtomicCounterThread(int32_t _num_iters, int32_t _num_threads, 
-			int32_t _thread_num) 
-	: num_iters(_num_iters), num_threads(_num_threads), 
-	  thread_num(_thread_num), final_sum(0) 
+    AtomicCounterThread(int32_t num_iters, int32_t num_threads, int32_t thread_num) 
+	: num_iters(num_iters), num_threads(num_threads), thread_num(thread_num), final_sum(0) 
     { }
 
     virtual ~AtomicCounterThread() { }
     
-    virtual void * run() {
+    virtual void *run() {
 	// Spin barrier -- be wary.
-	while(!doStart) {
+        while (!doStart) {
 	    sched_yield();
 	}
 
-	for(int32_t i=0; i<num_iters; i++){
+	for (int32_t i=0; i<num_iters; ++i) {
             // Excercise both increment interfaces: fetchThenAdd and
             // fetchThenInc
-	    if(thread_num==1) {
+	    if (thread_num == 1) {
 		correct_count.fetchThenInc();
 		incorrect_count++;
 	    } else {
@@ -45,7 +44,7 @@ public:
 		incorrect_count = val;
 	    }
             
-	    SINVARIANT(thread_num==0 || !correct_count.isZero());
+	    SINVARIANT(thread_num == 0 || !correct_count.isZero());
 	}
 	return 0;
     }
@@ -53,33 +52,36 @@ public:
     int32_t num_iters, num_threads, thread_num, final_sum;
 };
 
-// TODO-sprint-3 (0.25 day): This should be unnecessary. Remove once
-// program options is clean.
-void init(const std::vector<std::string> &args){
-}
-
-int main( ){
-    int32_t num_threads = 250;
-    int32_t num_iters = 10000;
+int main () {
+    // For this test, running on more than # cores doesn't have any
+    // benefit since our races are entirely from running threads.
+    int32_t num_threads = PThreadMisc::getNCpus(true);
+    if (num_threads == -1) {
+        num_threads = 32; // guess -- highish for circa 2009
+    }
+    int32_t num_iters = 1000 * 1000; // 1 million chances to screw up.
+    if (num_threads == 1) {
+        num_threads = 2;
+        num_iters = 1000 * 1000; // hope we will get swapped out at a good point
+    }
     // Determine "the answer" for final sum.
     int32_t final_sum =0;
-    for(int32_t i=0; i<num_threads; i++){
+    for(int32_t i=0; i<num_threads; ++i) {
 	final_sum += i;
     }
     final_sum *= num_iters; // "the answer"
     
-    PThread **thread_list = new PThread*[num_threads];
-    for(int32_t i=0; i<num_threads; i++){
-	thread_list[i] = 
-	    new AtomicCounterThread(num_iters, num_threads, i);
+    std::vector<PThread *> thread_list;
+    for(int32_t i=0; i<num_threads; ++i) {
+        thread_list.push_back(new AtomicCounterThread(num_iters, num_threads, i));
     }
 
     // Run them all at once
-    for(int32_t i=0; i<num_threads; i++){
+    for(int32_t i=0; i<num_threads; ++i) {
 	thread_list[i]->start();
     }
-    doStart=1;
-    for(int32_t i=0; i<num_threads; i++){
+    doStart = 1;
+    for(int32_t i=0; i<num_threads; ++i) {
 	thread_list[i]->join();
     }
 
@@ -91,6 +93,6 @@ int main( ){
     SINVARIANT(incorrect_count != final_sum);
 
     std::cout << incorrect_count << " vs " << final_sum << 
-	"(" << incorrect_count / (final_sum / 100.0) << "%)" << std::endl;
-    
+	"(" << incorrect_count / (final_sum / 100.0) << "%)" << "\n";
+    return 0;
 }
