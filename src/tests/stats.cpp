189476b536695b0486524ec54797b3c73a5e61ef
Eric Anderson <eric.anderson4@hp.com>
2009-08-05 20:58:06 +0000

    * Found seed that causes variance check to fail.
    Stats.cpp: remove useless cast
    
    tests/stats.cpp: add lots of extra debugging output and ability to control
      seed.  Document exactly the case that fails in the code.

--- src/tests/stats.cpp
+++ src/tests/stats.cpp
@@ -4,51 +4,57 @@
    See the file named COPYING for license details
 */
 
-
-#include <boost/format.hpp>
 #include <Lintel/AssertBoost.hpp>
 #include <Lintel/Double.hpp>
 #include <Lintel/MersenneTwisterRandom.hpp>
 #include <Lintel/Stats.hpp>
-#include <Lintel/ProgramOptions.hpp>
+#include <Lintel/StringUtil.hpp>
 
+using namespace std;
 using boost::format;
 
-lintel::ProgramOption<int> po_extra_reps
-("extra-reps", 
- "number of extra repetitions of testStatsMerging() to call", 
- 1);
+void checkEqual(Stats &a, Stats &b, uint32_t iters) {
+    INVARIANT(a.count() == b.count(), "count mismatch");
+    INVARIANT(a.min() == b.min(), "min mismatch");
+    INVARIANT(a.max() == b.max(), "max mismatch");
+    INVARIANT(Double::eq(a.mean(), b.mean()), "mean mismatch");
 
-void
-checkEqual(Stats &a, Stats &b)
-{
-    INVARIANT(a.count() == b.count(), 
-	      format("count mismatch %d!=%d") % a.count() % b.count());
-    INVARIANT(a.min() == b.min(), 
-	      format("min mismatch %f!=%f") % a.min() % b.min());
-    INVARIANT(a.max() == b.max(), 
-	      format("max mismatch %f!=%f") % a.max() % b.max());
-    INVARIANT(Double::eq(a.mean(), b.mean()), 
-	      format("mean mismatch %f!=%f") % a.mean() % b.mean());
-    INVARIANT(Double::eq(a.variance(), b.variance()), 
-	      format("variance mismatch %f!=%f, difference is %f")
-	      % a.variance() % b.variance() % (a.variance() - b.variance()));
+    // With seed 2069933494 on an Opteron 8220 SE with Debian etch
+    // 32bit, the following check fails after one iteration.  The
+    // error appears to be down in the last fp bit since the error is
+    // almost exactly 2^-53, and it just gets magnified up, so we fail
+    // to be equal to the default epsilon.
+    double var_a = a.total_sq()/static_cast<double>(a.count()) - a.mean() * a.mean();
+    double var_b = b.total_sq()/static_cast<double>(b.count()) - b.mean() * b.mean();
+
+    INVARIANT(Double::eq(a.variance(), b.variance(), 5.0e-12), 
+	      format("variance mismatch %.20g - %.20g = %.20g; %d iters\n"
+		     "(n,mean^2,sumsq): a=(%d,%.20g,%.20g); b=(%d,%.20g,%.20g)\n"
+		     "a-b=(%d,%.20g,%.20g) ;; %.20g - %.20g = %.20g") 
+	      % a.variance() % b.variance() % (a.variance() - b.variance()) % iters 
+	      % a.count() % (a.mean() * a.mean()) % a.total_sq()
+	      % b.count() % (b.mean() * b.mean()) % b.total_sq()
+	      % (a.count() - b.count()) % (a.mean() * a.mean() - b.mean() * b.mean())
+	      % (a.total_sq() - b.total_sq())
+	      % var_a % var_b % (var_a - var_b));
 }
 
-void
-checkInRange(unsigned long count, double min, double max)
-{
+void checkInRange(unsigned long count, double min, double max) {
     INVARIANT(count >= min && count <= max, "range error");
 }
 
-void
-testStatsMerging()
-{
+void testStatsMerging() {
     Stats base_1, base_2, merge;
 
     uint32_t n_random_values = 1000000;
 
     MersenneTwisterRandom rand; // Let it seed differently each time
+
+    if (getenv("STATS_SEED") != NULL) {
+	rand.init(stringToInteger<uint32_t>(getenv("STATS_SEED")));
+    }
+
+    cout << format("Seeded merge test with %d\n") % rand.seed_used;
 
     for(uint32_t i=0; i < n_random_values; ++i) {
 	double rv = rand.randDoubleOpen53();
@@ -58,15 +64,15 @@ testStatsMerging()
 	if (which_base) {
 	    base_1.add(rv);
 	    test.add(base_1);
-	    checkEqual(test, base_1);
+	    checkEqual(test, base_1, i);
 	    test.add(base_2);
-	    checkEqual(test, merge);
+	    checkEqual(test, merge, i);
 	} else {
 	    base_2.add(rv);
 	    test.add(base_2);
-	    checkEqual(test, base_2);
+	    checkEqual(test, base_2, i);
 	    test.add(base_1);
-	    checkEqual(test, merge);
+	    checkEqual(test, merge, i);
 	}
     }
 
@@ -78,11 +84,7 @@ testStatsMerging()
 int
 main(int argc, char *argv[])
 {
-    lintel::parseCommandLine(argc, argv);
-    
-    for(int i = 0; i<po_extra_reps.get(); ++i) {
-	// TODO: more tests
-	testStatsMerging();
-    }
+    // TODO: more tests
+    testStatsMerging();
     return 0;
 }
