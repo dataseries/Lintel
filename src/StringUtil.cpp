df84d319ae63c0ce59afc8a77b14608237f0f659
Eric Anderson <eric.anderson4@hp.com>
2009-08-09 02:07:39 +0000

    PThread.hpp.in: mark timedwait as deprecated.
    StringUtil.hpp: remove deprecated functions
    
    StringUtil.cpp: fix up to conform to coding convention

--- src/StringUtil.cpp
+++ src/StringUtil.cpp
@@ -40,9 +40,7 @@
 using namespace std;
 using boost::format;
 
-void 
-split(const string &instr, const string &splitstr, vector<string> &bits) 
-{
+void split(const string &instr, const string &splitstr, vector<string> &bits) {
     SINVARIANT(splitstr.length() > 0);
     int startbit = 0;
     while(true) {
@@ -59,9 +57,7 @@ split(const string &instr, const string &splitstr, vector<string> &bits)
     }
 }
 
-string
-join(const string &joinstr, const vector<string> &bits)
-{
+string join(const string &joinstr, const vector<string> &bits) {
     string ret;
 
     unsigned size = 0;
@@ -116,8 +112,7 @@ static unsigned char mysql_escape_table[256] =
           0,    0,    0,    0,    0,    0,    0,    0,    0,    0, // 24
           0,    0,    0,    0,    0,    0 } ;                      // 25
 
-string 
-mysqlEscape(const void * _data, unsigned datasize) {
+string mysqlEscape(const void * _data, unsigned datasize) {
     const unsigned char *data = reinterpret_cast<const unsigned char *>(_data);
     int to_escape = 0;
     for(unsigned int i=0; i<datasize; ++i) {
@@ -170,9 +165,7 @@ string maybehexstring(const string &a) {
 // writes a char string in CSV form (as it is accepted by Excel)
 // if the string has commas then we need to use "" around the string
 // if the string has " then we need to double the existent quotes 
-string 
-toCSVform(const string &s)
-{
+string toCSVform(const string &s) {
     string ret(s);
     int pos = -1; 
 
@@ -189,9 +182,7 @@ toCSVform(const string &s)
     return ret;
 }
 
-static int 
-unpackhexchar(const char v)
-{
+static int unpackhexchar(const char v) {
     if (v >= '0' && v <= '9')
 	return v - '0';
     if (v >= 'a' && v <= 'f')
@@ -200,17 +191,13 @@ unpackhexchar(const char v)
     return -1;
 }
 
-static unsigned char
-unpackhex(const char *v)
-{
+static unsigned char unpackhex(const char *v) {
     int v1 = unpackhexchar(v[0]);
     int v2 = unpackhexchar(v[1]);
     return (unsigned char)((v1 << 4) | v2);
 }
 
-string
-hex2raw(const string &in)
-{
+string hex2raw(const string &in) {
     INVARIANT((in.size() % 2) == 0,
 	      format("can't convert '%s' to hex, not an even number of digits")
 	      % in);
@@ -222,9 +209,7 @@ hex2raw(const string &in)
     return out;
 }
 
-string
-maybehex2raw(const string &in)
-{
+string maybehex2raw(const string &in) {
     if ((in.size() % 2) != 0) {
 	return in;
     }
@@ -236,23 +221,17 @@ maybehex2raw(const string &in)
     return hex2raw(in);
 }
 
-bool
-prefixequal(const string &str, const string &prefix)
-{
+bool prefixequal(const string &str, const string &prefix) {
     return str.compare(0,prefix.size(),prefix) == 0;
 }
 
-bool
-suffixequal(const string &str, const string &prefix)
-{
+bool suffixequal(const string &str, const string &prefix) {
     if (str.size() < prefix.size())
 	return false;
     return str.compare(str.size() - prefix.size(),prefix.size(),prefix) == 0;
 }
 
-string
-ipv4tostring(uint32_t val)
-{
+string ipv4tostring(uint32_t val) {
     return boost::str(boost::format("%d.%d.%d.%d")
 		      % ((unsigned int)(val >> 24) & 0xFF)
 		      % ((unsigned int)(val >> 16) & 0xFF)
@@ -263,9 +242,7 @@ ipv4tostring(uint32_t val)
 // TODO: in_addr_t and friends under windows needs to be figured out
 #ifdef SYS_POSIX
 
-uint32_t
-stringtoipv4(const string &val)
-{
+uint32_t stringtoipv4(const string &val) {
     in_addr_t addr;
     INVARIANT((addr = inet_addr(val.c_str())) != (in_addr_t)-1,
 	      boost::format("Unable to convert %s to inet address")
@@ -275,9 +252,7 @@ stringtoipv4(const string &val)
 
 #endif // SYS_POSIX
 
-double
-stringToDouble(const string &str)
-{
+double stringToDouble(const string &str) {
     char *endptr = NULL;
     INVARIANT(str.size() > 0, 
 	      "zero length string not valid in conversion to double");
@@ -319,8 +294,7 @@ bool stringIsSpace(const string &str)
 // not working on windows or cygwin?
 #ifdef SYS_POSIX
 #ifndef __CYGWIN__
-string getHostFQDN()
-{
+string getHostFQDN() {
     char buf[1024];
     buf[sizeof(buf) - 1] = '\0';
     int rc = gethostname(buf, sizeof(buf) - 1);
@@ -356,8 +330,7 @@ string getHostFQDN()
 #endif
 #endif
 
-string stringError(int errnum)
-{
+string stringError(int errnum) {
     const size_t buflen = 256;
     char buf[buflen];
 
@@ -380,8 +353,7 @@ string stringError(int errnum)
 #endif
 }
 
-string downcaseString(const string &s)
-{
+string downcaseString(const string &s) {
     string r(s);
     string::size_type len = r.length();
     
@@ -391,8 +363,7 @@ string downcaseString(const string &s)
     return r;
 }
 
-string upcaseString(const string &s)
-{
+string upcaseString(const string &s) {
     string r(s);
     string::size_type len = r.length();
     
@@ -405,8 +376,7 @@ string upcaseString(const string &s)
 
 // TODO: see if we need to do something to support wstring on cygwin
 #ifndef __CYGWIN__
-wstring string2wstring(const string &s, const locale &loc)
-{
+wstring string2wstring(const string &s, const locale &loc) {
     wstring out;
     out.reserve(s.size());
     const ctype<wchar_t> &ct = use_facet<ctype<wchar_t> >(loc);
@@ -415,8 +385,7 @@ wstring string2wstring(const string &s, const locale &loc)
     return out;
 }
 
-string wstring2string(const wstring &s, char dfault, const locale &loc)
-{
+string wstring2string(const wstring &s, char dfault, const locale &loc) {
     string out;
     out.reserve(s.size());
     const ctype<wchar_t> &ct = use_facet<ctype<wchar_t> >(loc);
