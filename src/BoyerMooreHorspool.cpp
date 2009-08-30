ae7664dedc37a3021ef4d223adba5a20342b0ac8
Kevin Smathers <ks1@hpl.hp.com>
2009-08-30 01:16:13 +0000

    Re-tabified

--- src/BoyerMooreHorspool.cpp
+++ src/BoyerMooreHorspool.cpp
@@ -22,50 +22,51 @@
 // at a time
 
 namespace lintel {
-namespace {
-    uint8_t *memDup(const void *in, size_t len) {
-	uint8_t *ret = new uint8_t[len];
-	memcpy(ret, in, len);
-	return ret;
-    }
-}
-
-BoyerMooreHorspool::BoyerMooreHorspool(const void *needle_v, size_t needle_length) 
-    : needle(memDup(needle_v, needle_length)), needle_length(needle_length), 
-      last(needle_length - 1) 
-{
-    INVARIANT(needle_length > 0, "invalid to search for zero length string");
-
-    // initialize the bad character shift array
-    for (uint32_t i = 0; i <= uint8_max; ++i) {
-        bad_char_shift[i] = needle_length;
+    namespace {
+	uint8_t *memDup(const void *in, size_t len) {
+	    uint8_t *ret = new uint8_t[len];
+	    memcpy(ret, in, len);
+	    return ret;
+	}
     }
 
-    for (size_t i = 0; i < last; ++i) {
-        bad_char_shift[needle[i]] = last - i;
+    BoyerMooreHorspool::BoyerMooreHorspool(const void *needle_v, size_t needle_length) 
+	: needle(memDup(needle_v, needle_length)), needle_length(needle_length), 
+	  last(needle_length - 1) 
+    {
+	INVARIANT(needle_length > 0, "invalid to search for zero length string");
+
+	// initialize the bad character shift array
+	for (uint32_t i = 0; i <= uint8_max; ++i) {
+	    bad_char_shift[i] = needle_length;
+	}
+
+	for (size_t i = 0; i < last; ++i) {
+	    bad_char_shift[needle[i]] = last - i;
+	}
     }
-}
 
-BoyerMooreHorspool::~BoyerMooreHorspool() {
-    delete [] needle;
-}
-
-
-ssize_t BoyerMooreHorspool::find(const void *hay_v, size_t hay_len) const {
-    const uint8_t *haystack = reinterpret_cast<const uint8_t *>(hay_v);
-
-    while (hay_len >= needle_length) {
-        for (size_t i = last; haystack[i] == needle[i]; --i) {
-            if (i == 0) { // first char matches so it's a match!
-                return (haystack - reinterpret_cast<const uint8_t *>(hay_v));
-            }
-        }
-	
-        size_t skip = bad_char_shift[haystack[last]];
-	//TODO-code-review: invariant was wrong.
-        hay_len -= skip;
-        haystack += skip;
+    BoyerMooreHorspool::~BoyerMooreHorspool() {
+	delete [] needle;
     }
-    return -1;
-}
+
+
+    ssize_t BoyerMooreHorspool::find(const void *hay_v, size_t hay_len) const {
+	const uint8_t *haystack = reinterpret_cast<const uint8_t *>(hay_v);
+
+	while (hay_len >= needle_length) {
+	    for (size_t i = last; haystack[i] == needle[i]; --i) {
+		if (i == 0) { // first char matches so it's a match!
+		    return (haystack - reinterpret_cast<const uint8_t *>(hay_v));
+		}
+	    }
+	    
+	    size_t skip = bad_char_shift[haystack[last]];
+	    //TODO-code-review: invariant was wrong.
+	    if (hay_len < skip) break;
+	    hay_len -= skip;
+	    haystack += skip;
+	}
+	return -1;
+    }
 }
