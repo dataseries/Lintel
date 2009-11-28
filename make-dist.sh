7e5fac9ec5caeafccabe995315bbdecab12fef98
Eric Anderson <eric.anderson4@hp.com>
2009-11-28 04:35:59 +0000

    make-dist.sh: re-enable local build.  Fix up typos in remote host testing.
    mercury-plot.in: re-enable reading from stdin, slightly more careful now,
      if stdin is a terminal, we open it as a terminal.  If it's not, we try to
      process command line arguments then stdin, and exit.  All the places where
      we use $term are now protected.
    
    mercury-plot.sh: add tests for reading from stdin and printing out manpage.

--- make-dist.sh
+++ make-dist.sh
@@ -54,67 +54,67 @@ fi
 LOG=/tmp/make-dist/log
 NOW=`date +%Y-%m-%d`
 
-## [ ! -d /tmp/make-dist ] || rm -rf /tmp/make-dist
-## mkdir /tmp/make-dist /tmp/make-dist/tar /tmp/make-dist/log
-## cp $0 /tmp/make-dist
-## cd /tmp/make-dist/tar
-## 
-## [ -f tmp.db ] || mtn -d tmp.db db init
-## touch pull.log
-## for i in $PACKAGES; do
-##     echo "Pulling $i from usi.hpl.hp.com; logging to /tmp/make-dist/pull.log..."
-## 
-##     cp $HOME/.monotone/ssd.db tmp.db
-## #    mtn -d tmp.db pull $MTN_PULL_FROM ssd.hpl.hp.com/$i >>pull.log 2>&1
-##     HEAD="`mtn -d tmp.db automate heads ssd.hpl.hp.com/$i`"
-##     REF_HEAD="`cd ~/projects/$i; mtn automate heads`"
-##     if [ "$HEAD" != "$REF_HEAD" ]; then 
-## 	echo "Weird, heads differ between synced ($HEAD) and ~/projects/$i ($REF_HEAD)"
-## 	exit 1
-##     fi
-## done
-## 
-## for i in $PACKAGES; do
-##     echo "Packaging $i: "
-##     [ ! -d $i-$NOW ] || rm -rf $i-$NOW
-##     mtn -d tmp.db co -b ssd.hpl.hp.com/$i $i-$NOW >../log/co.$i 2>&1
-##     cd $i-$NOW
-##     mtn log >Changelog.mtn
-##     echo "Monotone-Revision: `mtn automate get_base_revision_id`" >Release.info
-##     echo "Creation-Date: $NOW" >>Release.info
-##     echo "   current revision is `grep Monotone-Revision Release.info | awk '{print $2}'`"
-##     cd ..
-##     echo "   tarfile; building $i-$NOW.tar.bz2; logging to /tmp/make-dist/tar.$i.log"
-##     tar cvvfj $i-$NOW.tar.bz2 --exclude=_MTN $i-$NOW >tar.$i.log 2>&1
-##     echo "   done: `ls -l $i-$NOW.tar.bz2`"
-## done
-## 
-## # Local build
-## echo "setup files..."
-## cp $BUILD_OPT/Lintel/src/deptool-bootstrap /tmp/make-dist
-## cp Lintel-$NOW.tar.bz2 /tmp/make-dist
-## cp DataSeries-$NOW.tar.bz2 /tmp/make-dist
-## 
-## echo "Copy to tesla (bg)..."
-## echo "Lintel-$NOW.tar.bz2" >/tmp/make-dist/latest-release
-## echo "DataSeries-$NOW.tar.bz2" >>/tmp/make-dist/latest-release
-## 
-## rsync -av --progress /tmp/make-dist/Lintel-$NOW.tar.bz2 /tmp/make-dist/DataSeries-$NOW.tar.bz2 /tmp/make-dist/latest-release tesla.hpl.hp.com:opensource/tmp >$LOG/rsync 2>&1 &
-## 
-## echo "Local build..."
-## /tmp/make-dist/make-dist.sh --test-local $NOW localhost >$LOG/build.localhost 2>&1
-## 
-## cp /tmp/make-dist/build/Lintel/src/deptool-bootstrap /tmp/make-dist
-## 
-## echo "Waiting for copy to finish..."
-## wait
+[ ! -d /tmp/make-dist ] || rm -rf /tmp/make-dist
+mkdir /tmp/make-dist /tmp/make-dist/tar /tmp/make-dist/log
+cp $0 /tmp/make-dist
+cd /tmp/make-dist/tar
+
+[ -f tmp.db ] || mtn -d tmp.db db init
+touch pull.log
+for i in $PACKAGES; do
+    echo "Pulling $i from usi.hpl.hp.com; logging to /tmp/make-dist/pull.log..."
+
+    cp $HOME/.monotone/ssd.db tmp.db
+#    mtn -d tmp.db pull $MTN_PULL_FROM ssd.hpl.hp.com/$i >>pull.log 2>&1
+    HEAD="`mtn -d tmp.db automate heads ssd.hpl.hp.com/$i`"
+    REF_HEAD="`cd ~/projects/$i; mtn automate heads`"
+    if [ "$HEAD" != "$REF_HEAD" ]; then 
+	echo "Weird, heads differ between synced ($HEAD) and ~/projects/$i ($REF_HEAD)"
+	exit 1
+    fi
+done
+
+for i in $PACKAGES; do
+    echo "Packaging $i: "
+    [ ! -d $i-$NOW ] || rm -rf $i-$NOW
+    mtn -d tmp.db co -b ssd.hpl.hp.com/$i $i-$NOW >../log/co.$i 2>&1
+    cd $i-$NOW
+    mtn log >Changelog.mtn
+    echo "Monotone-Revision: `mtn automate get_base_revision_id`" >Release.info
+    echo "Creation-Date: $NOW" >>Release.info
+    echo "   current revision is `grep Monotone-Revision Release.info | awk '{print $2}'`"
+    cd ..
+    echo "   tarfile; building $i-$NOW.tar.bz2; logging to /tmp/make-dist/tar.$i.log"
+    tar cvvfj $i-$NOW.tar.bz2 --exclude=_MTN $i-$NOW >tar.$i.log 2>&1
+    echo "   done: `ls -l $i-$NOW.tar.bz2`"
+done
+
+# Local build
+echo "setup files..."
+cp $BUILD_OPT/Lintel/src/deptool-bootstrap /tmp/make-dist
+cp Lintel-$NOW.tar.bz2 /tmp/make-dist
+cp DataSeries-$NOW.tar.bz2 /tmp/make-dist
+
+echo "Copy to tesla (bg)..."
+echo "Lintel-$NOW.tar.bz2" >/tmp/make-dist/latest-release
+echo "DataSeries-$NOW.tar.bz2" >>/tmp/make-dist/latest-release
+
+rsync -av --progress /tmp/make-dist/Lintel-$NOW.tar.bz2 /tmp/make-dist/DataSeries-$NOW.tar.bz2 /tmp/make-dist/latest-release tesla.hpl.hp.com:opensource/tmp >$LOG/rsync 2>&1 &
+
+echo "Local build..."
+/tmp/make-dist/make-dist.sh --test-local $NOW localhost >$LOG/build.localhost 2>&1
+
+cp /tmp/make-dist/build/Lintel/src/deptool-bootstrap /tmp/make-dist
+
+echo "Waiting for copy to finish..."
+wait
 
 echo "starting test-host builds..."
 
 for host in $TEST_HOSTS; do
-    echo "Starting on $host; log to /tmp/make-dist/$host.log"
+    echo "Starting on $host; log to $LOG/$host.log"
     echo -n "  copy..."
-    scp $0 $host:/tmp/make-dist.sh >$LOG/$host.log 2>&1
+    scp /tmp/make-dist/make-dist.sh $host:/tmp/make-dist.sh >$LOG/$host.log 2>&1
     echo -n "  prepare..."
     ssh $host /tmp/make-dist.sh --prepare >$LOG/$host.log 2>&1
     echo -n "  copy..."
@@ -136,12 +136,12 @@ wait
 echo "verifying test-host builds succeeded."
 for host in $TEST_HOSTS; do
     echo -n "$host: fetch..."
-    echo "No-File-Copied" >$LOG/ok-$host
+    echo "No-File-Copied" >$LOG/result-$host
     scp $host:/tmp/make-dist/result-$host $LOG/result-$host >>$LOG/$host.log 2>&1 || true
     if [ "`head -1 $LOG/result-$host`" = "$NOW" ]; then
 	echo "ok"
     else
-	echo "failed, ok-$host doesn't contain $NOW, but '`head -1 $LOG/ok-$HOST`'"
+	echo "failed, ok-$host doesn't contain $NOW, but '`head -1 $LOG/result-$HOST`'"
 	exit 1
     fi
 done
--- src/tests/mercury-plot.sh
+++ src/tests/mercury-plot.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 echo quit >tmp.hg
+../mercury-plot <tmp.hg
 ../mercury-plot tmp.hg
+../mercury-plot --man
+
 
