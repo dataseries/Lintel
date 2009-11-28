981650d0d765b85f44692dcd8e8c1a8112413049
Eric Anderson <eric.anderson4@hp.com>
2009-11-28 03:36:01 +0000

    * Yet more distribution fixups
    CMakeLists.txt: move cmake policy settings into top level cmake file
    
    make-dist.sh: Add prepare option to do all the initial preparation steps.
      unset no_proxy and http_proxy to clear out any environment state.
      Rename ok-<host> to result-<host>; it may not be ok.  Temporary comment out
      of local build.  More output as we run through the remote steps.
      Make sure to specify log directory everywhere.
    
    deptool.in: Add debug option and remove debugging output.  Fix use of
      wget, the -O <output> needs to come first.

--- make-dist.sh
+++ make-dist.sh
@@ -4,6 +4,15 @@ TEST_HOSTS="keyvalue-debian-x86-2.u.hpl.hp.com pds-big-1.u.hpl.hp.com hplxc.hpl.
 [ "$MTN_PULL_FROM" = "" ] && MTN_PULL_FROM=usi.hpl.hp.com
 
 set -e
+
+if [ "$1" = "--prepare" -a "$0" = "/tmp/make-dist.sh" ]; then
+    [ ! -d /tmp/make-dist ] || mv /tmp/make-dist /tmp/make-dist.rm 
+    [ ! -d /tmp/make-dist.rm ] || rm -rf /tmp/make-dist.rm 
+    mkdir /tmp/make-dist 
+    mv $0 /tmp/make-dist/make-dist.sh
+    exit 0
+fi
+
 if [ "$1" = "--test-local" -a "$2" != "" -a "$3" != "" ]; then
     PATH=/tmp/make-dist/bin:$PATH
     cd /tmp/make-dist
@@ -22,116 +31,120 @@ if [ "$1" = "--test-wget" -a "$2" != "" -a "$3" != "" ]; then
     cd /tmp/make-dist
     export PROJECTS=/tmp/make-dist/projects
     export BUILD_OPT=/tmp/make-dist/build
+    unset http_proxy
+    unset no_proxy
     case $3 in
-	*) http_proxy=http://web-proxy.corp.hp.com:8088/;
+	*.hp.com) http_proxy=http://web-proxy.corp.hp.com:8088/;
 	   export http_proxy ;;
     esac
-    perl /tmp/make-dist/deptool-bootstrap tarinit http://tesla.hpl.hp.com/opensource/tmp/latest-release
+    perl /tmp/make-dist/deptool-bootstrap --debug tarinit http://tesla.hpl.hp.com/opensource/tmp/latest-release
     cd $PROJECTS/DataSeries
     perl /tmp/make-dist/deptool-bootstrap build -t
     echo "MAKE-DIST: EVERYTHING OK!"
-    echo $2 >ok-$3
+    echo $2 >result-$3
     exit 0
 fi
 
 if [ "$1" != "" ]; then
+    echo "Unexpected argument '$1' '$2' '$3'"
     echo "Usage: ./make-dist"
     exit 1
 fi
 
-[ ! -d /tmp/make-dist ] || rm -rf /tmp/make-dist
-mkdir /tmp/make-dist /tmp/make-dist/tar /tmp/make-dist/log
-cp $0 /tmp/make-dist
-cd /tmp/make-dist/tar
-
-[ -f tmp.db ] || mtn -d tmp.db db init
-touch pull.log
-for i in $PACKAGES; do
-    echo "Pulling $i from usi.hpl.hp.com; logging to /tmp/make-dist/pull.log..."
-
-    cp $HOME/.monotone/ssd.db tmp.db
-#    mtn -d tmp.db pull $MTN_PULL_FROM ssd.hpl.hp.com/$i >>pull.log 2>&1
-    HEAD="`mtn -d tmp.db automate heads ssd.hpl.hp.com/$i`"
-    REF_HEAD="`cd ~/projects/$i; mtn automate heads`"
-    if [ "$HEAD" != "$REF_HEAD" ]; then 
-	echo "Weird, heads differ between synced ($HEAD) and ~/projects/$i ($REF_HEAD)"
-	exit 1
-    fi
-done
-
+LOG=/tmp/make-dist/log
 NOW=`date +%Y-%m-%d`
-for i in $PACKAGES; do
-    echo "Packaging $i: "
-    [ ! -d $i-$NOW ] || rm -rf $i-$NOW
-    mtn -d tmp.db co -b ssd.hpl.hp.com/$i $i-$NOW >../log/co.$i 2>&1
-    cd $i-$NOW
-    mtn log >Changelog.mtn
-    echo "Monotone-Revision: `mtn automate get_base_revision_id`" >Release.info
-    echo "Creation-Date: $NOW" >>Release.info
-    echo "   current revision is `grep Monotone-Revision Release.info | awk '{print $2}'`"
-    cd ..
-    echo "   tarfile; building $i-$NOW.tar.bz2; logging to /tmp/make-dist/tar.$i.log"
-    tar cvvfj $i-$NOW.tar.bz2 --exclude=_MTN $i-$NOW >tar.$i.log 2>&1
-    echo "   done: `ls -l $i-$NOW.tar.bz2`"
-done
 
-# Local build
-echo "setup files..."
-cp $BUILD_OPT/Lintel/src/deptool-bootstrap /tmp/make-dist
-cp Lintel-$NOW.tar.bz2 /tmp/make-dist
-cp DataSeries-$NOW.tar.bz2 /tmp/make-dist
-
-echo "Copy to tesla (bg)..."
-echo "Lintel-$NOW.tar.bz2" >/tmp/make-dist/latest-release
-echo "DataSeries-$NOW.tar.bz2" >>/tmp/make-dist/latest-release
-
-rsync -av --progress /tmp/make-dist/Lintel-$NOW.tar.bz2 /tmp/make-dist/DataSeries-$NOW.tar.bz2 /tmp/make-dist/latest-release tesla.hpl.hp.com:opensource/tmp >/tmp/make-dist/log/rsync 2>&1 &
-
-echo "Local build..."
-/tmp/make-dist/make-dist.sh --test-local $NOW localhost >/tmp/make-dist/log/build.localhost 2>&1
-
-cp /tmp/make-dist/build/Lintel/src/deptool-bootstrap /tmp/make-dist
-
-echo "Waiting for copy to finish..."
-wait
+## [ ! -d /tmp/make-dist ] || rm -rf /tmp/make-dist
+## mkdir /tmp/make-dist /tmp/make-dist/tar /tmp/make-dist/log
+## cp $0 /tmp/make-dist
+## cd /tmp/make-dist/tar
+## 
+## [ -f tmp.db ] || mtn -d tmp.db db init
+## touch pull.log
+## for i in $PACKAGES; do
+##     echo "Pulling $i from usi.hpl.hp.com; logging to /tmp/make-dist/pull.log..."
+## 
+##     cp $HOME/.monotone/ssd.db tmp.db
+## #    mtn -d tmp.db pull $MTN_PULL_FROM ssd.hpl.hp.com/$i >>pull.log 2>&1
+##     HEAD="`mtn -d tmp.db automate heads ssd.hpl.hp.com/$i`"
+##     REF_HEAD="`cd ~/projects/$i; mtn automate heads`"
+##     if [ "$HEAD" != "$REF_HEAD" ]; then 
+## 	echo "Weird, heads differ between synced ($HEAD) and ~/projects/$i ($REF_HEAD)"
+## 	exit 1
+##     fi
+## done
+## 
+## for i in $PACKAGES; do
+##     echo "Packaging $i: "
+##     [ ! -d $i-$NOW ] || rm -rf $i-$NOW
+##     mtn -d tmp.db co -b ssd.hpl.hp.com/$i $i-$NOW >../log/co.$i 2>&1
+##     cd $i-$NOW
+##     mtn log >Changelog.mtn
+##     echo "Monotone-Revision: `mtn automate get_base_revision_id`" >Release.info
+##     echo "Creation-Date: $NOW" >>Release.info
+##     echo "   current revision is `grep Monotone-Revision Release.info | awk '{print $2}'`"
+##     cd ..
+##     echo "   tarfile; building $i-$NOW.tar.bz2; logging to /tmp/make-dist/tar.$i.log"
+##     tar cvvfj $i-$NOW.tar.bz2 --exclude=_MTN $i-$NOW >tar.$i.log 2>&1
+##     echo "   done: `ls -l $i-$NOW.tar.bz2`"
+## done
+## 
+## # Local build
+## echo "setup files..."
+## cp $BUILD_OPT/Lintel/src/deptool-bootstrap /tmp/make-dist
+## cp Lintel-$NOW.tar.bz2 /tmp/make-dist
+## cp DataSeries-$NOW.tar.bz2 /tmp/make-dist
+## 
+## echo "Copy to tesla (bg)..."
+## echo "Lintel-$NOW.tar.bz2" >/tmp/make-dist/latest-release
+## echo "DataSeries-$NOW.tar.bz2" >>/tmp/make-dist/latest-release
+## 
+## rsync -av --progress /tmp/make-dist/Lintel-$NOW.tar.bz2 /tmp/make-dist/DataSeries-$NOW.tar.bz2 /tmp/make-dist/latest-release tesla.hpl.hp.com:opensource/tmp >$LOG/rsync 2>&1 &
+## 
+## echo "Local build..."
+## /tmp/make-dist/make-dist.sh --test-local $NOW localhost >$LOG/build.localhost 2>&1
+## 
+## cp /tmp/make-dist/build/Lintel/src/deptool-bootstrap /tmp/make-dist
+## 
+## echo "Waiting for copy to finish..."
+## wait
 
 echo "starting test-host builds..."
 
 for host in $TEST_HOSTS; do
     echo "Starting on $host; log to /tmp/make-dist/$host.log"
+    echo -n "  copy..."
+    scp $0 $host:/tmp/make-dist.sh >$LOG/$host.log 2>&1
     echo -n "  prepare..."
-    ssh $host mv /tmp/make-dist /tmp/make-dist.rm >$host.log 2>&1
-    ssh $host rm -rf /tmp/make-dist.rm >$host.log 2>&1
-    ssh $host mkdir /tmp/make-dist >>$host.log 2>&1
-    scp $0 $host:/tmp/make-dist >>$host.log 2>&1
-    scp /tmp/make-dist/deptool-bootstrap $host:/tmp/make-dist >>$host.log 2>&1
+    ssh $host /tmp/make-dist.sh --prepare >$LOG/$host.log 2>&1
+    echo -n "  copy..."
+    scp /tmp/make-dist/deptool-bootstrap $host:/tmp/make-dist/deptool-bootstrap >>$LOG/$host.log 2>&1
 
     echo "start build."
 
-    ssh $host /tmp/make-dist/make-dist.sh --test-wget $NOW $host >>$host.log 2>&1 &
+    ssh $host /tmp/make-dist/make-dist.sh --test-wget $NOW $host >>$LOG/$host.log 2>&1 &
 done
-
-wait
 
 # echo "schroot builds..."
 # for i in etch-32bit lenny-32bit; do
 #     echo "$i..."
-#     schroot -c $i -- /tmp/make-dist/make-dist.sh --test-local $NOW $i >/tmp/make-dist/log/$i 2>&1
+#     schroot -c $i -- /tmp/make-dist/make-dist.sh --test-local $NOW $i >$LOG/$i 2>&1
 # done
+
+wait
 
 echo "verifying test-host builds succeeded."
 for host in $TEST_HOSTS; do
     echo -n "$host: fetch..."
-    scp $host:/tmp/make-dist/ok-$host ok-$host >>$host.log 2>&1
-    if [ "`cat ok-$host`" = "$NOW" ]; then
+    echo "No-File-Copied" >$LOG/ok-$host
+    scp $host:/tmp/make-dist/result-$host $LOG/result-$host >>$LOG/$host.log 2>&1 || true
+    if [ "`head -1 $LOG/result-$host`" = "$NOW" ]; then
 	echo "ok"
     else
-	echo "failed, ok-$host doesn't contain $NOW"
+	echo "failed, ok-$host doesn't contain $NOW, but '`head -1 $LOG/ok-$HOST`'"
 	exit 1
     fi
 done
-
-
 
 exit 0
 
