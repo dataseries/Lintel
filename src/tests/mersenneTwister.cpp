f60b5b615401dc93b7cecb40833ded56201e7450
Eric Anderson <eric.anderson4@hp.com>
2010-02-11 06:22:50 +0000

    dependencies.tex: add the dependencies needed for lintel-latex-rebuild
    MersenneTwisterRandom.hpp: add TODO to replace this with the well documented
      Fisher-Yates shuffle, rather than the undocumented one we used (same algorithm
      as in the g++ stl).
    
    mersenneTwister.cpp: write code to print out the results of shuffle operation.

--- src/tests/mersenneTwister.cpp
+++ src/tests/mersenneTwister.cpp
@@ -9,14 +9,41 @@
 */
 
 
-#include <stdio.h>
+#include <iostream>
+#include <boost/format.hpp>
+
+using namespace std;
 
 #include <Lintel/MersenneTwisterRandom.hpp>
 
-int 
-main()
-{
+void shuffleShow() {
+    vector<int> distr;
+    vector<int> base;
+    distr.resize(10);
+    base.resize(distr.size());
+    for(uint32_t i = 0; i < base.size(); ++i) {
+	base[i] = i;
+    }
+
+    MersenneTwisterRandom rng;
+
+    vector<int> work;
+    work.resize(base.size());
+    for(uint32_t i = 0; i < 100000; ++i) {
+	work = base;
+	MT_random_shuffle(work.begin(), work.end(), rng);
+	distr[work[0]] += 1;
+    }
+
+    for(uint32_t i = 0; i < distr.size(); ++i) {
+	cout << boost::format("%d -> %d\n") % i % distr[i];
+    }
+}
+
+int main() {
     MersenneTwisterRandom::selfTest();
+
+    //    shuffleShow();
     return 0;
 }
 
