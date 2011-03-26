0a262977cfb83f6e1797c8d87c0fc48be561df3d
Eric Anderson <eric.anderson4@hp.com>
2011-03-26 21:49:45 +0000

    * Add packaging for redhat; as part of that, fix mercury-plot to not   require IO::Pty or Data::ShowTable so we can build it successfully   on redhat.
    make-rpm.sh: replaced with redhat/rules; latter allows for partial rebuild during testing.
    
    fix-install.sh: factor out the code that fixes up the install after cmake puts in
      rpath's that we don't want.
    
    copy-tar.sh: script to copy the generated tar file to the rpm source directory
    
    get-version.sh: script to extract the current version from the debian changelog, and
      the release from the redhat spec file.  This approach guarantees consistent versioning, 
      between our two main styles of releases assuming we always try to rebuild both.
    
    make-tar.sh: Create both the Lintel.spec file and the Lintel-<version>.tar.gz files
    
    patch-spec.pl: Patch up the .spec file to replace the __*__ bits.  In particular we keep
      the descriptions consistent between debian and redhat.
    
    rules: makefile for building the rpm.
    
    CMakeConfig.txt: Remove with of IO::Pty; nothing uses it now.
      Change mercury-plot dependencies to just generate a warning if Data::ShowTable is
      not present; it will work, just not quite as well.
    
    debian/changelog: update changelog message, packaging should work on ubuntu also, so
      call it dpkg packaging.
    
    debian/control: Add dependency on liblintel-perl to lintel-utils
    
    debian/rules: Factor out the code that fixes up after the install into fix-install.sh; 
      remove the obsolete configure call.
    
    Lintel.spec.in: Tons of changes to get the packaging description working again and to
      split things into lots of separate packages.
    
    src/mercury-plot.in: Remove the dependency on IO::Pty and Data::ShowTable; the former is
      removed by using a different method for synchronizing with gnuplot -- we now have it
      execute a command to create a file when it has synchronized.  The latter by having a 
      csv mode that does not require the showtable module.

--- redhat/make-tar.sh
+++ redhat/make-tar.sh
@@ -26,39 +26,24 @@ if [ ! -f Release.info -o ! -f Changelog.mtn ]; then
     exit 1
 fi
 
-REL_DATE=`grep Creation-Date Release.info | awk '{print $2}'`
-REL_VERSION=0.`echo $REL_DATE | sed 's/-/./g'`
-sed "s/__VERSION__/$REL_VERSION/" <redhat/Lintel.spec.in >redhat/Lintel.spec
+. redhat/get-version.sh
+perl redhat/patch-spec.pl $VERSION $RELEASE
 
-VERSION=`grep Version: redhat/Lintel.spec | awk '{print $2}'`
-if [ "$VERSION" = "" ]; then
+CHECK_VERSION=`grep Version: redhat/Lintel.spec | awk '{print $2}'`
+if [ "$CHECK_VERSION" = "" -o "$CHECK_VERSION" = "0." ]; then
     echo "Missing version in Lintel.spec"
     exit 1
 fi
 
-if [ "$VERSION" != "$REL_VERSION" ]; then
-    echo "Bad version in redhat/Lintel.spec; $VERSION != $REL_VERSION"
+if [ "$CHECK_VERSION" != "$VERSION" ]; then
+    echo "Bad version in redhat/Lintel.spec; $CHECK_VERSION != $VERSION"
     exit 1
 fi
-
-rpm_topdir=`grep '^._topdir ' $HOME/.rpmmacros | awk '{print $2}'`
-
-[ "$rpm_topdir" == "" ] && rpm_topdir=/usr/src/redhat
 
 cwd=`pwd`
 dir=`basename $cwd`
 cd ..
-[ "$dir" == "Lintel-$VERSION" ] || mv $dir Lintel-$VERSION
-tar cvvfz $rpm_topdir/SOURCES/Lintel-$VERSION.tar.gz Lintel-$VERSION
-[ "$dir" == "Lintel-$VERSION" ] || mv Lintel-$VERSION $dir 
-cd $dir
+[ "$dir" == "Lintel-$VERSION" ] || ln -s $dir Lintel-$VERSION
+tar cvvfhz Lintel-$VERSION.tar.gz Lintel-$VERSION/
+[ "$dir" == "Lintel-$VERSION" ] || rm Lintel-$VERSION
 
-rpmbuild -ba redhat/Lintel.spec || exit 1
-
-if [ -d _MTN ]; then
-    rm Release.info
-    rm Changelog.mtn
-    rm redhat/Lintel.spec
-fi
-
-echo "SUCCESS: built rpm package"
