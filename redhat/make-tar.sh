588af699fccd3dccf8df80097079bee52243e2a1
Eric Anderson <eric.anderson4@hp.com>
2011-03-28 07:31:33 +0000

    * Now we pass rpmlint (with two exceptions)
    Lintel.spec.in: Sundry changes so that we can pass rpmlint.  Mainly 
      a lot of adding README files, moving around the packages for a few 
      files, and adding post scripts to call ldconfig.
    
    make-tar.sh: [BUGFIX] exit if patch-spec.pl fails don't blindly continue.

--- redhat/make-tar.sh
+++ redhat/make-tar.sh
@@ -27,7 +27,7 @@ if [ ! -f Release.info -o ! -f Changelog.mtn ]; then
 fi
 
 . redhat/get-version.sh
-perl redhat/patch-spec.pl $VERSION $RELEASE
+perl redhat/patch-spec.pl $VERSION $RELEASE || exit 1
 
 CHECK_VERSION=`grep Version: redhat/Lintel.spec | awk '{print $2}'`
 if [ "$CHECK_VERSION" = "" -o "$CHECK_VERSION" = "0." ]; then
