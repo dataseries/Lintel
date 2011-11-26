ec381bc73d8170abd45b72c86636cfbff0fd8ac3
Eric Anderson <eric.anderson4@hp.com>
2011-11-26 22:27:09 +0000

    * Add support for git commit.  Refactored so that it should be straightforward to add additional      version control systems.
    deptool.in: Add TODO to move to using more modules to speedup commands.  Start fixing the use of
      $monotone_dir.  Add another git config option to control how we do diffs.  Print message
      explaining why we are reconfiguring the global git config if needed.  Refactor the deptool
      ignores code.  Fix up the init/checkout code to not use mirror and to set up our three way
      mode for publish (pull from central, push to local, push to remote).  Refactor the commit code
      to use the new VCInterface stuff.
    
    make-deptool-bootstrap.sh: [BUGFIX] was keeping around the BEGIN { push(@INC, "eric's libdir"); }
      code because of incorrect regex.

--- src/make-deptool-bootstrap.sh
+++ src/make-deptool-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-grep -v 'BEGIN.*INC.*build/opt' deptool | grep -v 'use Lintel' >deptool-bootstrap.tmp
+grep -v 'BEGIN.*dir.*build/opt.*unshift.*INC' deptool | grep -v 'use Lintel' >deptool-bootstrap.tmp
 # If you update the next line, you should update the dependencies in src/CMakeLists.txt
 cat $1/Lintel/File/Lock.pm $1/Lintel/ProcessManager.pm >>deptool-bootstrap.tmp
 mv deptool-bootstrap.tmp deptool-bootstrap
